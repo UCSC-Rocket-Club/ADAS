76475eae3f2fd5cd5fa7e0d64069abfbb9b8bef8
Demo User <debian@beaglebone.localdomain>
2018-12-08 15:33:35 +0000

    fucking python shit

--- MotorDriver/test.py
+++ MotorDriver/test.py
@@ -5,8 +5,8 @@ import os
 Cprogram = subprocess.Popen(['./motorDriver'],stdout=open('adae.txt','w'), stdin=subprocess.PIPE, preexec_fn=os.setsid)
 
 while(1):
-    inme = input("gimmie a position")
-    Cprogram.stdin.write(str(inme))
+        inme = input("gimmie a position")
+        Cprogram.stdin.write(str(inme))
 	Cprogram.stdin.flush()
 #	A = Cprogram.stdout.readline().strip()
 #	print(A)
